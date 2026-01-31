490c54b87bed15b0f1bedd472d26121e10f01a30
AtharvaW03 <113792733+AtharvaW03@users.noreply.github.com>
2026-01-31 19:52:54 +0530

    Fix sq-ch3-ex5 as regular folder

---
+++ ch3/sq-ch3-ex5/src/main/java/com/sprint/Person.java
@@ -0,0 +1,26 @@
+package com.sprint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name = "Atharva";
+
+    private final Parrot parrot;
+
+    @Autowired
+    public Person(Parrot parrot){
+        this.parrot = parrot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+
+
+    }
+}
