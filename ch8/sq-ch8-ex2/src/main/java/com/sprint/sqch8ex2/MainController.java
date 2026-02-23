11bdece2e879bd935d2dc99bd89111c7ab35b578
AtharvaW03 <atharva@example.com>
2026-02-23 23:44:06 +0530

    Re-add ch7 and ch8 from local springpractice

---
+++ ch8/sq-ch8-ex2/src/main/java/com/sprint/sqch8ex2/MainController.java
@@ -0,0 +1,24 @@
+package com.sprint.sqch8ex2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @RequestMapping("/home")
+    public String home(@RequestParam(required = false) String name,
+                       @RequestParam(required = false) String color,
+                       Model page){
+
+        if (name != null) {
+            page.addAttribute("username", name);
+        }
+        if (color != null) {
+            page.addAttribute("color", color);
+        }
+
+        return "home.html";
+    }
+}
