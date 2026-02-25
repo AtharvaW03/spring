045413605d9121b960eeca4df741a8335c4f606e
AtharvaW03 <113792733+AtharvaW03@users.noreply.github.com>
2026-02-25 23:50:21 +0530

    Add ch10 projects: sq-ch10-ex1 and sq-ch10-ex3

---
+++ ch10/sq-ch10-ex3/src/main/java/com/sprint/sqch10ex3/CountryController.java
@@ -0,0 +1,14 @@
+package com.sprint.sqch10ex3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/france")
+    public Country france(){
+        Country country = Country.of("France", 67);
+        return country;
+    }
+}
