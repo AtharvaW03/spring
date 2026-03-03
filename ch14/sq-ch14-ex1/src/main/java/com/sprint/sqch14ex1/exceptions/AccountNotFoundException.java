17d90a5a7dfbff18d52f92048d762a17f4bf4ec8
AtharvaW03 <113792733+AtharvaW03@users.noreply.github.com>
2026-03-03 09:19:22 +0530

    Revert "Add ch14 sq-ch14-ex1 project"
    This reverts commit 5ee995840df7a11618b75721aca0e9ba5b4ca12a.

--- ch14/sq-ch14-ex1/src/main/java/com/sprint/sqch14ex1/exceptions/AccountNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.sprint.sqch14ex1.exceptions;
-
-public class AccountNotFoundException extends RuntimeException {
-
-}
--- ch14/sq-ch14-ex1/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO account (name, amount) VALUES ('John Doe', 1000);
-INSERT INTO account (name, amount) VALUES ('Jack Smith', 1500);
